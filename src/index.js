74392118f50676387acb11f01f24a35e9a1e5a7f
Valentin Anikin <anival-94@mail.ru>
2020-10-01 20:14:39 +0300

    feat: add all tasks solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (arguments.length === 0) {
+    return 0
+  }
+  if (array.length === 0) {
+    return 0
+  }
+  let i = array.length;
+  let minNumber = Infinity;
+  while (i >= 0) {
+    if (array[i] < minNumber) {
+      minNumber = array[i]
+    }
+    i -= 1;
+  }
+  return minNumber
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (arguments.length === 0) {
+    return 0
+  }
+  if (array.length === 0) {
+    return 0
+  }
+  return Math.max.apply(null, array)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (arguments.length === 0) {
+    return 0
+  }
+  if (array.length === 0) {
+    return 0
+  }
+  let sum = array.reduce((a, x) => a += x);
+  return sum / array.length;
 }
